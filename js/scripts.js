25039d7a2d56b5163006e14c761706c4c0dbaff8
Daniel <danielhernandez9980@gmail.com>
2020-11-25 12:54:59 -0600

    Archivos base y menu responsive

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+    window.addEventListener('scroll', ()=>{
+        const header = document.querySelector('header');
+        header.classList.toggle('sticky', scrollY>0);
+    })
+   
+
+    function toggleMenu(){
+        const menu = document.querySelector('.menu');
+        const toggle = document.querySelector('.toggle');
+
+        menu.classList.toggle('active');
+        toggle.classList.toggle('active');
+}
